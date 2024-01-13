99abb3ab823e161fe7910b11a8dc4bf1dc58ab8e
Andrew Cheung <andrewcheung360@gmail.com>
2024-01-12 23:55:50 -0500

    removed unused actions and created script files

--- src/app/actions/authActions.ts
+++ src/app/actions/authActions.ts
@@ -1,54 +1,6 @@
 'use server'
-import { headers, cookies } from 'next/headers'
+import { cookies } from 'next/headers'
 import { createClient } from '../../../utils/supabase/server'
-import { redirect } from 'next/navigation'
-
-export async function emailSignin(formData : FormData){
-    const email = formData.get('email') as string
-    const password = formData.get('password') as string
-    const cookieStore = cookies()
-    const supabase = createClient(cookieStore)
-
-    const { error } = await supabase.auth.signInWithPassword({
-      email: email,
-      password: password,
-    })
-    if (error) {
-        return redirect('/')
-      }
-  
-    return redirect('/dashboard')
-}
-
-export async function emailSignup(formData : FormData){
-  const origin = headers().get('origin')
-  const email = formData.get('email') as string
-  const password = formData.get('password') as string
-  const cookieStore = cookies()
-  const supabase = createClient(cookieStore)
-
-  const { error } = await supabase.auth.signUp({
-    email,
-    password,
-    options: {
-      emailRedirectTo: `${origin}/auth/callback`,
-    },
-  })
-
-    if (error) {
-      console.log(error)
-        return redirect('/')
-      }
-  
-    return redirect('/dashboard')
-}
-
-export async function signOut(){
-  const cookieStore = cookies()
-  const supabase = createClient(cookieStore)
-  await supabase.auth.signOut()
-  return redirect('/')
-}
 
 export async function getUser(){
   const cookieStore = cookies()
