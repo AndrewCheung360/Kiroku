f2c11b656f0271253b6650f393c450a4be11cff0
Andrew Cheung <andrewcheung360@gmail.com>
2024-01-20 23:59:08 -0500

    added lodash and framer-motion, also added anime fetching function

--- src/app/actions/authActions.ts
+++ src/app/actions/authActions.ts
@@ -8,3 +8,4 @@ export async function getUser(){
   const user = (await supabase.auth.getSession()).data.session?.user
   return user
 }
+
--- src/app/anime/page.tsx
+++ src/app/anime/page.tsx
@@ -1,10 +1,27 @@
-export default function Anime(){
-    return(
-        <>
-        <div>
-            
-            Anime
-        </div>
-        </>
-    )
+'use client'
+import { useState, useEffect, useRef } from "react";
+import { createClient } from "../../../utils/supabase/client";
+import { debounce } from "lodash";
+import { motion } from "framer-motion";
+const supabase = createClient();
+
+
+export async function getAnime(){
+    const { data, error } = await supabase
+      .from('anime')
+      .select('*')
+      .order('mal_score', { ascending: false })
+      .limit(20)
+    return data
+  }
+
+export default function AnimePage() {
+  const anime = getAnime();
+  console.log(anime)
+  return (
+    <></>
+  )
+
+  
 }
+
